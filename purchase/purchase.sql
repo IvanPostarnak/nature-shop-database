6cd8604fc1e9f61647f0307deac01999381f88e5
Ivan Postarnak <ivan.postarnak.s@gmail.com>
2023-05-25 03:14:42 +0300

    feat: add 'product_rating' table and add small fix of 'purchase' table

--- purchase/purchase.sql
+++ purchase/purchase.sql
@@ -4,12 +4,13 @@ CREATE TABLE purchase
 (
 	purchase_id INTEGER GENERATED ALWAYS AS IDENTITY,
   customer_id INTEGER NOT NULL,
-  employee_id INTEGER NOT NULL,
+  employee_id INTEGER,
   shop_id INTEGER NOT NULL,
   date DATE NOT NULL DEFAULT NOW(),
   time TIME NOT NULL DEFAULT NOW(),
   
 	CONSTRAINT pk_purchase_purchase_id PRIMARY KEY (purchase_id),
+  CONSTRAINT chk_purchase_employee_id_shop_id CHECK (shop_id != 1 OR employee_id IS NULL),
   CONSTRAINT fk_purchase_customer_id_customer_customer_id FOREIGN KEY (customer_id) REFERENCES customer(customer_id),
   CONSTRAINT fk_purchase_employee_id_employee_employee_id FOREIGN KEY (employee_id) REFERENCES employee(employee_id),
   CONSTRAINT fk_purchase_shop_id_shop_shop_id FOREIGN KEY (shop_id) REFERENCES shop(shop_id)
