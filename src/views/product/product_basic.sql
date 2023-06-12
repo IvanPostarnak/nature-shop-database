59d3aab767afa3341e082b3dfabe18870637a63e
Ivan Postarnak <ivan.postarnak.s@gmail.com>
2023-06-12 09:48:04 +0300

    feat: add materialized view 'product_basic'

---
+++ src/views/product/product_basic.sql
@@ -0,0 +1,19 @@
+DROP MATERIALIZED VIEW product_basic;
+
+CREATE MATERIALIZED VIEW product_basic AS
+     SELECT product_id,
+            product.name,
+            company.abbr as company_abbr,
+            brand.name as brand_name,
+            product_points.points_inner as points,
+            product_price.consumer_price as price,
+            product_rating.rating_5 as rating,
+            product_rating.votes_number_5 as votes_count
+       FROM product
+  LEFT JOIN company USING (company_id)
+  LEFT JOIN brand USING (brand_id)
+  LEFT JOIN product_points USING (product_id)
+  LEFT JOIN product_price USING (product_id)
+  LEFT JOIN product_rating USING (product_id);
+
+SELECT * FROM product_basic;
