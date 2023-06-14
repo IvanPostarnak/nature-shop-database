20bce2d79b669344cde6784a2f52dfb20e4d3257
Ivan Postarnak <ivan.postarnak.s@gmail.com>
2023-06-14 05:33:04 +0300

    fix: add join of needed tables for products' views, delete 'functions/'

--- src/views/product/product_basic.sql
+++ src/views/product/product_basic.sql
@@ -3,8 +3,8 @@ DROP MATERIALIZED VIEW product_basic;
 CREATE MATERIALIZED VIEW product_basic AS
      SELECT product_id,
             product.name,
-            company.abbr as company_abbr,
-            brand.name as brand_name,
+            company.abbr as company,
+            brand.name as brand,
             product_points.points_inner as points,
             product_price.consumer_price as price,
             product_rating.rating_5 as rating,
--- src/views/product/product_everything.sql
+++ src/views/product/product_everything.sql
@@ -7,7 +7,11 @@ CREATE MATERIALIZED VIEW product_everything AS
             (
               SELECT ROW_TO_JSON(company_table)
                 FROM (
-                  SELECT company.*
+                  SELECT company_id,
+                         company.name AS name,
+                         company.abbr AS abbr,
+                         company.description AS description,
+                         country.name AS country
                     FROM company
                     JOIN country USING (country_id)
                    WHERE company_id = product.company_id
@@ -16,7 +20,9 @@ CREATE MATERIALIZED VIEW product_everything AS
             (
               SELECT ROW_TO_JSON(brand_table)
                 FROM (
-                  SELECT brand_id, name, description
+                  SELECT brand_id,
+                         name,
+                         description
                     FROM brand
                    WHERE brand_id = product.brand_id
                 ) AS brand_table
@@ -24,7 +30,9 @@ CREATE MATERIALIZED VIEW product_everything AS
             (
               SELECT ROW_TO_JSON(rating_table)
                 FROM (
-                  SELECT product_rating_id AS rating_id, rating_5 AS value, votes_number_5 AS votes_count
+                  SELECT product_rating_id AS rating_id,
+                         rating_5 AS value,
+                         votes_number_5 AS votes_count
                     FROM product_rating
                    WHERE product_id = product.product_id
                 ) AS rating_table
@@ -32,7 +40,11 @@ CREATE MATERIALIZED VIEW product_everything AS
             (
               SELECT ROW_TO_JSON(price_table)
                 FROM (
-                  SELECT product_price_id AS price_id, diller_price AS diller, distr_price AS distr, wholesale_price AS wholesale, consumer_price AS consumer
+                  SELECT product_price_id AS price_id,
+                         diller_price AS diller,
+                         distr_price AS distr,
+                         wholesale_price AS wholesale,
+                         consumer_price AS consumer
                     FROM product_price
                    WHERE product_id = product.product_id
                 ) AS price_table
@@ -40,7 +52,10 @@ CREATE MATERIALIZED VIEW product_everything AS
             (
               SELECT ROW_TO_JSON(points_table)
                 FROM (
-                  SELECT product_point_id AS points_id, points_outer AS outer, points_inner AS inner, points_discount AS discount
+                  SELECT product_point_id AS points_id,
+                         points_outer AS outer,
+                         points_inner AS inner,
+                         points_discount AS discount
                     FROM product_points
                    WHERE product_id = product.product_id
                 ) AS points_table
@@ -48,23 +63,42 @@ CREATE MATERIALIZED VIEW product_everything AS
             (
               SELECT ROW_TO_JSON(size_table)
                 FROM (
-                  SELECT product_size_id AS size_id, weight_g AS weight, length_cm AS length, width_cm AS width, height_cm AS height, packing_material_id
+                  SELECT product_size_id AS size_id,
+                         weight_g AS weight,
+                         length_cm AS length,
+                         width_cm AS width,
+                         height_cm AS height,
+                         packing_material.name AS packing_material
                     FROM product_size
+                    JOIN packing_material USING (packing_material_id)
                    WHERE product_id = product.product_id
                 ) AS size_table
             ) AS size,
             (
               SELECT ROW_TO_JSON(data_table)
                 FROM (
-                  SELECT product_data_id AS data_id, product_form_id, has_dispenser, total_volume, items_total, item_volume, measure_unit_id
+                  SELECT product_data_id AS data_id,
+                         product_form_id,
+                         has_dispenser,
+                         total_volume,
+                         items_total,
+                         item_volume,
+                         measure_unit.abbr AS measure_unit
                     FROM product_data
+                    JOIN measure_unit USING (measure_unit_id)
                    WHERE product_id = product.product_id
                 ) AS data_table
             ) AS data,
 		 	      (
               SELECT ROW_TO_JSON(info_table)
                 FROM (
-                  SELECT product_info_id AS info_id, pros, advantages, active_ingredients, composition, application, contraindications, storing
+                  SELECT product_info_id AS info_id,
+                         pros, advantages,
+                         active_ingredients,
+                         composition,
+                         application,
+                         contraindications,
+                         storing
                     FROM product_info
                    WHERE product_id = product.product_id
                 ) AS info_table
